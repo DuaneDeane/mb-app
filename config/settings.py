8c790e86ad27a78df2b999d4d139b9568ebf9636
Duane Deane <lilboom@Duanes-Air.attlocal.net>
2020-12-09 19:40:07 -0600

    updated with bootstrap

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts'
+    'bootstrap4',
+    'fontawesome_5',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = [str(BASE_DIR.joinpath('static'))]
